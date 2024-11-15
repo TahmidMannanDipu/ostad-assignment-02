3cea5837be54296839e6328c1bc8d79f5d46ef8b
Tahmid Mannan Dipu <143392225+TahmidMannanDipu@users.noreply.github.com>
2024-11-15 22:14:26 +0600

    liveTest 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MyApp());
+
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Need Blood",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.black54,
+                radius: 80,
+                child: Icon(
+                  Icons.bloodtype,
+                  size: 100,
+                  color: Colors.red,
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "Donate Blood",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
