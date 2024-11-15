e01ed82d106b77f2f2d9fc92f744acc0315f4b73
Tahmid Mannan Dipu <143392225+TahmidMannanDipu@users.noreply.github.com>
2024-11-15 21:52:12 +0600

    liveTest 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,14 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
+    return MaterialApp(
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // build reusable widget
-    Widget buildIconAndText(IconData icon, String text) {
-      return Padding(
-        padding: const EdgeInsets.only(top: 16.0, left: 8),
-        child: Column(
-          children: [
-            CircleAvatar(
-              backgroundColor: Colors.deepPurpleAccent[50],
-              radius: 90,
-              child: Icon(
-                icon,
-                size: 100,
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              text,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("My Profile"),
-        backgroundColor: Colors.yellow[700],
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: buildIconAndText(Icons.icecream_outlined,
-                  "Ice cream is very delicious right?"),
-            ),
-            Expanded(
-              child: buildIconAndText(
-                  Icons.code, "Programming is not boring if you love it"),
-            ),
-            Expanded(
-                child: buildIconAndText(Icons.egg_outlined,
-                    "If you submit code directly copy from chatgpt then mark will 0"))
-          ],
-        ),
-      ),
     );
   }
 }
